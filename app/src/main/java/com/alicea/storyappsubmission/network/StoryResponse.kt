76dc7f7856b8c9d4d645eead7f751839518bd82d
Aisha Atiko <aisya_h@outlook.com>
2022-11-20 01:26:21 +0700

    rename package

--- app/src/main/java/com/alicea/storyappsubmission/network/StoryResponse.kt
+++ app/src/main/java/com/alicea/storyappsubmission/network/StoryResponse.kt
@@ -1,5 +1,6 @@
-package com.alicea.storyappsubmission.networking
+package com.alicea.storyappsubmission.network
 
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 data class FileUploadResponse(
@@ -21,8 +22,43 @@ data class StoryListResponse(
     val listStory: List<ListStoryItem>
 )
 
-
 data class ListStoryItem(
+    @PrimaryKey
+    @field:SerializedName("id")
+    val id: String,
+
+    @field:SerializedName("name")
+    val name: String,
+
+    @field:SerializedName("description")
+    val description: String,
+
+    @field:SerializedName("photoUrl")
+    val photoUrl: String,
+
+    @field:SerializedName("createdAt")
+    val createdAt: String,
+
+    @field:SerializedName("lat")
+    val lat: String? = null,
+
+    @field:SerializedName("lon")
+    val lon: String? = null
+)
+
+data class StoryLocationListResponse(
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("listStory")
+    val listStory: List<ListStoryLocationItem>
+)
+
+data class ListStoryLocationItem(
+    @PrimaryKey
     @field:SerializedName("id")
     val id: String,
 
